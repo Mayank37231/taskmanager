05718230571eec69828afb8c046be96bbe490397
Mayank37231 <76561491+Mayank37231@users.noreply.github.com>
2025-09-15 14:52:00 +0530

    urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import  TokenRefreshView
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("delete-account/", UserDeleteView.as_view(), name="delete-account"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("tasks/", TaskListCreateView.as_view(), name="task-list-create"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
